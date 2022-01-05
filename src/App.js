45292fed3cc23d6db4fb99f7899567506f7d15ac
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-01-05 01:45:17 -0300

    components, props and state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+ const [showImage, setShowImage] = useState(false);
+ const toggle = () => {
+   setShowImage(!showImage);
+ }
+  
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button type="button"onClick={toggle}>
+            {this.state.showImage ? 'Esconder' : 'Mostrar' }
+            </button>
+            
+          {this.state.showImage && (
+             <img src={logo} className="App-logo" alt="logo" />
+          )}
+        </header>
+      </div>
+    );
+  }
+
+
+ 
 
 export default App;
